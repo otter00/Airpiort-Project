fce4ca2bda4e3bd75b6a622ceb4815e673463a68
otter00 <ivlevanastia2001@mail.ru>
2022-12-06 21:21:14 +0300

    strict log-in

--- js/main.js
+++ js/main.js
@@ -11,31 +11,69 @@
 //     }
 // }
 
-function validateForm(form) {
 
-    var nameField = form.username;
-    var passLine = form.pass;
+/*  Функция-запасной аэродром */ 
 
-    if (isNotEmpty(nameField)) {
-        if(isNotEmpty(passLine)) {
-            return true;
-        }
-    }
-    return false;
-}
+function validateForm(applicantForm) {
 
-function isNotEmpty(field) {
+    var name = document.getElementById("login__input").value; // read name field value by id
+    //var nameField = form.username;
+    //var passLine = form.pass;
 
-    var fieldData = field.value;
-
-    if (fieldData.length == 0 || fieldData == "") {
-        //field.className = "FieldError"; //Class to highlight error
+    if (name != "anastasia") {
         alert("Incorrect");
-        //return false;
     } else {
-        //field.className = "You successfully authorized"; //Resets field back to default
         alert("You successfully authorized");
         window.location.href = 'htmls/airportOVB.html';
-        //return true; //Submits form
     }
+
+    // if (isNotEmpty(nameField)) {
+    //     if(isNotEmpty(passLine)) {
+    //         return true;
+    //     }
+    // }
+    // return false;
 }
+
+// function isNotEmpty(field) {
+
+//     var fieldData = field.value;
+
+//     if (fieldData.length == 0 || fieldData == "") {
+//         //field.className = "FieldError"; //Class to highlight error
+//         alert("Incorrect");
+//         //return false;
+//     } else {
+//         //field.className = "You successfully authorized"; //Resets field back to default
+//         alert("You successfully authorized");
+//         window.location.href = 'htmls/airportOVB.html';
+//         //return true; //Submits form
+//     }
+// }
+
+// function handleFormSubmit(event) {
+//     // here we ask the form to prevent autosending
+//     event.preventDefault()
+//     console.log('Sending!')
+// }
+
+
+function serializeForm(formNode) { // formNode is a specific form DOM-element
+    const { elements } = formNode // elements is a method contains control units and fields
+    const data = Array.from(elements) // convert elements into array
+        .filter((item) => !!item.name) // filter whether elem has empty name
+        .map((element) => { // map collects an array with elements have name and value
+        const { name, value } = element
+
+        return { name, value }
+    })
+    console.log(data)
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault() // here we ask the form to prevent autosending and refreshing
+    serializeForm(applicantForm)
+}
+
+const applicantForm = document.getElementById('login__form') // our form
+applicantForm.addEventListener('submit', handleFormSubmit) // check when we click submit button to send data and refresh page
